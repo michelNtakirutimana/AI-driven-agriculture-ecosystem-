d098b62d720dddf6fd87928fcb3e5f8bf230d974
michelNtakirutimana <Michelntakirutimana810@gmail.com>
2026-01-29 18:25:08 +0200

    Add files via upload

---
+++ Mike t.py
@@ -0,0 +1,12 @@
+izina=input("andka izina ryawe: ")
+ibiro=int(input("uyumusi winjijeibiro bingahe? "))
+inyongera=ibiro*0.2
+umusaruro_vubaha=ibiro+inyongera
+print(f"muraho  {izina}, ejo biteganyijwe ko umusaruro wawe uzaba ari ibiro {umusaruro_vubaha} kg")
+if ibiro <100:
+      print("inama :  umusaruro wawe urihasi .koresha ifumbire yongera imbaraga")
+elif  ibiro <= 200: 
+      print("inama : umusaruro wawe urashimishije ! tangira gushaka isoko ryokugurishamo") 
+else:
+     print ("ongera ygerageze")
+print("-" * 20)
